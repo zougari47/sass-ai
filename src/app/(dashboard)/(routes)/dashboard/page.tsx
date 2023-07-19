373454bdcd59626fa1fe6f7c941017d6b20db052
unknown <hello@ahmedzougari.com>
2023-07-19 19:51:13 +0100

    first commit

---
+++ src/app/(dashboard)/(routes)/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import { Button } from '@/components/ui/button'
+
+const DashboardPage = () => {
+  return (
+    <div className="container">
+      <p>Dashboard Page</p>
+    </div>
+  )
+}
+
+export default DashboardPage
---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const HomePage = () => {
+  return <div className="text-4xl text-center">This is the home page</div>
+}
+
+export default HomePage
