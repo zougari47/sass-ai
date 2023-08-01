15a5ad4ad4b79b997ac53cff736fdc0c575d3996
Ahmed Zougari <zougariahmed007@gmail.com>
2023-08-01 10:45:17 +0100

    finish music generation

---
+++ src/app/(dashboard)/(routes)/music/constants.ts
@@ -0,0 +1,9 @@
+import * as z from 'zod'
+
+export const formSchema = z.object({
+  prompt: z.string().min(1, {
+    message: 'Music Prompt is required',
+  }),
+})
+
+export type IFormSchema = z.infer<typeof formSchema>
