bd15210b7af5cf97f8b47deceaecf98e5704c82d
Ahmed Zougari <zougariahmed007@gmail.com>
2023-08-15 15:40:03 +0100

    Finish video ai

---
+++ src/app/(dashboard)/(routes)/video/page.tsx
@@ -0,0 +1,110 @@
+'use client'
+
+import Empty from '@/components/Empty'
+import Heading from '@/components/Heading'
+import Loader from '@/components/Loader'
+import { Button } from '@/components/ui/button'
+import { Form, FormControl, FormField, FormItem } from '@/components/ui/form'
+import { Input } from '@/components/ui/input'
+import { zodResolver } from '@hookform/resolvers/zod'
+import axios from 'axios'
+import { VideoIcon } from 'lucide-react'
+import { useRouter } from 'next/navigation'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+import { IFormSchema, formSchema } from './constants'
+
+const VideoPage = () => {
+  const router = useRouter()
+  const [video, setVideo] = useState<string>()
+  const form = useForm<IFormSchema>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      prompt: '',
+    },
+  })
+
+  const isLoading = form.formState.isSubmitting
+
+  const onSubmit = async (values: IFormSchema) => {
+    try {
+      setVideo(undefined)
+
+      const response = await axios.post('/api/video', values)
+
+      setVideo(response.data[0])
+    } catch (error) {
+      console.log(error)
+    } finally {
+      router.refresh()
+    }
+  }
+
+  return (
+    <div>
+      <Heading
+        title="Video Generation"
+        description="Turn your prompt into video."
+        icon={VideoIcon}
+        iconColor="text-orange-700"
+        bgColor="bg-orange-700/10"
+      />
+
+      <div className="px-4 lg:px-8">
+        <div>
+          <Form {...form}>
+            <form
+              onSubmit={form.handleSubmit(onSubmit)}
+              className="rounded-lg border w-full p-4 px-3 md:px-6 focus-within:shadow-sm grid grid-cols-12 gap-2"
+            >
+              <FormField
+                name="prompt"
+                render={({ field }) => (
+                  <FormItem className="col-span-12 lg:col-span-10">
+                    <FormControl className="m-0 p-0">
+                      <Input
+                        className="border-0 outline-none focus-visible:ring-0 focus-visible:ring-transparent"
+                        disabled={isLoading}
+                        placeholder="Clown fish swimming around a coral reef"
+                        {...field}
+                      />
+                    </FormControl>
+                  </FormItem>
+                )}
+              />
+
+              <Button
+                className="col-span-12 lg:col-span-2 w-full"
+                disabled={isLoading}
+                type="submit"
+              >
+                Generate
+              </Button>
+            </form>
+          </Form>
+        </div>
+
+        <div className="space-y-4 mt-4">
+          {isLoading && (
+            <div className="p-8 rounded-lg w-full flex items-center justify-center bg-muted">
+              <Loader />
+            </div>
+          )}
+
+          {!video && !isLoading && <Empty label="No video generated." />}
+
+          {video && (
+            <video
+              className="w-full aspect-video mt-8 rounded-lg border bg-black"
+              controls
+            >
+              <source src={video} />
+            </video>
+          )}
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default VideoPage
