c0f813335d3632f72c1378b94dacac6265b69780
Ahmed Zougari <zougariahmed007@gmail.com>
2023-08-15 17:04:17 +0100

    WIP free trial limit

---
+++ src/lib/api-limit.ts
@@ -0,0 +1,48 @@
+import { auth } from '@clerk/nextjs'
+import db from '@/lib/db'
+import { MAX_FREE_COUNTS } from '@/constants'
+
+export const increaseApiLimit = async () => {
+  const { userId } = auth()
+
+  if (!userId) return
+
+  const userApiLimit = await db.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  })
+
+  if (userApiLimit) {
+    await db.userApiLimit.update({
+      where: {
+        userId,
+      },
+      data: {
+        count: userApiLimit.count + 1,
+      },
+    })
+  } else {
+    await db.userApiLimit.create({
+      data: {
+        userId,
+        count: 1,
+      },
+    })
+  }
+}
+
+export const checkApiLimit = async () => {
+  const { userId } = auth()
+
+  if (!userId) return false
+
+  const userApiLimit = await db.userApiLimit.findUnique({
+    where: {
+      userId,
+    },
+  })
+
+  if (!userApiLimit || userApiLimit.count < MAX_FREE_COUNTS) return true
+  else return false
+}
