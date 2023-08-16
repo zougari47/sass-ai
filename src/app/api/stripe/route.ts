66e397d768614889d4cbf9b96e81997cdee1e059
Ahmed Zougari <zougariahmed007@gmail.com>
2023-08-16 16:58:36 +0100

    setup webhook

---
+++ src/app/api/stripe/route.ts
@@ -0,0 +1,65 @@
+import { auth, currentUser } from '@clerk/nextjs'
+import { NextResponse } from 'next/server'
+
+import db from '@/lib/db'
+import { stripe } from '@/lib/stripe'
+import { absoluteUrl } from '@/lib/utils'
+
+const settingsUrl = absoluteUrl('/settings')
+
+export async function GET(req: Request) {
+  try {
+    const { userId } = auth()
+    const user = await currentUser()
+
+    if (!userId || !user) {
+      return new NextResponse('Unauthorized', { status: 401 })
+    }
+
+    const userSubscription = await db.userSubscription.findUnique({
+      where: {
+        userId,
+      },
+    })
+
+    if (userSubscription && userSubscription.stripeCustomerId) {
+      const stripeSession = await stripe.billingPortal.sessions.create({
+        customer: userSubscription.stripeCustomerId,
+        return_url: settingsUrl,
+      })
+
+      return new NextResponse(JSON.stringify({ url: stripeSession.url }))
+    }
+
+    const stripeSession = await stripe.checkout.sessions.create({
+      success_url: settingsUrl,
+      cancel_url: settingsUrl,
+      payment_method_types: ['card'],
+      mode: 'subscription',
+      billing_address_collection: 'auto',
+      customer_email: user.emailAddresses[0].emailAddress,
+      line_items: [
+        {
+          price_data: {
+            currency: 'USD',
+            product_data: {
+              name: 'Genius Pro',
+              description: 'Unlimited AI Generations',
+            },
+            unit_amount: 2000,
+            recurring: {
+              interval: 'month',
+            },
+          },
+          quantity: 1,
+        },
+      ],
+      metadata: {
+        userId,
+      },
+    })
+  } catch (error) {
+    console.log('[STRIPE_ERROR]', error)
+    return new NextResponse('Stripe Error', { status: 500 })
+  }
+}
